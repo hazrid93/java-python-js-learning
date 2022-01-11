c3b93b97920b645ac8398514200e5665551adeed
0xSaiya <hazrid93@hotmail.com>
2022-01-11 08:32:54 +0800

    Update cuda-test.py

--- machine-learning/cuda-test.py
+++ machine-learning/cuda-test.py
@@ -1,5 +1,7 @@
+# To verify Tensorflow are able to use GPU properly
+
 import tensorflow as tf
 
-print(tf.test.is_built_with_cuda())
+print(tf.test.is_built_with_cuda()) # should print 'True' without any error
 
 print(tf.config.list_physical_devices('GPU'))
